0830661a923b20f8129b88fcdcac63e087f8f545
Le Minh Hieu <hieubeo0612@gmail.com>
2021-03-25 00:50:03 +0700

    Update

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-let btn=document.getElementById('btn')
-let p=document.getElementsByTagName('p')
-let h1=document.getElementsByTagName('h1')
-btn.addEventListener('click',heart)
-function heart(){
-    for(let x of p){
-    x.className='opacity'
-    }
-    h1[0].className='opacity'
+let p = document.getElementsByTagName('p')
+let h1 = document.getElementById('title')
+let yes = document.getElementById('yes')
+let no = document.getElementById('no')
+yes.value = 'Có'
+no.value = 'Không'
+no.addEventListener('click', swap)
+yes.addEventListener('click', heart)
+function swap() {
+    yes.id = 'no'
+    no.id = 'yes'
+    yes = document.getElementById('yes')
+    no = document.getElementById('no')
+    yes.value = 'Có'
+    no.value = 'Không'
+    console.log(no);
 }
-
+function heart() {
+    for (let x of p) {
+        x.className = 'animate__animated animate__fadeInUp'
+    }
+    h1.className='animate__animated animate__slideInDown'
+    h1.innerHTML='❤️Meeeee tooooo❤️'
+    yes.className = 'no-opacity'
+    no.className = 'no-opacity'
+}
